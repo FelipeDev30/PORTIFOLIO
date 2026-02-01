59334478907185d5a36d0f0f68b6643f0b57ae1d
= <=>
2026-02-01 03:00:26 -0300

    Foi adicionado a sessão de skills e realizado o aprimoramento do código js para melhorar a funcionalidade dos botões

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
-const button = document.querySelector('.trigger');
+const button = document.querySelectorAll('.trigger');
 
-button.addEventListener('click', () => {
-    document.querySelector('.content').classList.toggle('active');
-    document.querySelector('.arrow').classList.toggle('rotate');
-});
+button.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const targetId = btn.getAttribute('data-id');
+        
+        // Fechar todas as outras sessões
+        document.querySelectorAll('.content').forEach(content => {
+            if (content.id !== targetId) {
+                content.classList.remove('active');
+            }
+        });
+        
+        document.querySelectorAll('.arrow').forEach(arrow => {
+            if (arrow.getAttribute('data-id') !== targetId) {
+                arrow.classList.remove('rotate');
+            }
+        });
+        
+        // Toggle apenas a sessão correspondente
+        document.getElementById(targetId).classList.toggle('active');
+        document.querySelector(`.arrow[data-id="${targetId}"]`).classList.toggle('rotate');
+    });
+})
